bd25e803cbf963822a15de13e86fdf2c19395718
Administrator <Administrator@PC-20181025JKKP>
2020-09-03 15:54:47 +0800

    提交框架模板初始项目

---
+++ C0-S3-Ply-cloud-base/src/main/java/com/yc/demo/C0S3Plybase/C0S3PlyBaseApplication.java
@@ -0,0 +1,13 @@
+package com.yc.demo.C0S3Plybase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C0S3PlyBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C0S3PlyBaseApplication.class, args);
+	}
+
+}
---
+++ C0-S3-Ply-cloud-parent1/src/test/java/com/yc/demo/C0S3Plybase/C0S3PlyBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yc.demo.C0S3Plybase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C0S3PlyBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
